a7bd7b27790f5566518a86443a0fd88e1d662b63
Kritarth Upadhyay <KUpadhyay@walmartlabs.com>
2015-11-04 00:25:48 -0800

    adding mentions

--- app/src/main/java/com/codepath/apps/MySimpleTweets/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/MySimpleTweets/TwitterClient.java
@@ -48,7 +48,7 @@ public class TwitterClient extends OAuthBaseClient {
 		String apiUrl = getApiUrl("statuses/home_timeline.json");
 		// Can specify query string params directly or through RequestParams.
 		RequestParams params = new RequestParams();
-		params.put("count", 15);
+		params.put("count",15);
 		params.put("since_id", maxId);
         Log.d("DEBUG", "getHomePageTimeline" + maxId);
         client.get(apiUrl, params, handler);
